581a6c2c30bd579af4c7fd54c76660c8e7440d8c
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-01 21:24:51 -0400

    02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, number) {
+  let updatedString = "";
+  if (number >= 0) {
+    for (let i = 0; i < number; i++) {
+      updatedString += string;
+    }
+  } else {
+    updatedString = "ERROR";
+  }
+  return updatedString;
 };
 
 // Do not edit below this line
