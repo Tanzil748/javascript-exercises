0f2083f4bd39a558dcb2623d0c292aa19e2569f8
Tanzil Hassan <tanzilhassan333@gmail.com>
2022-08-06 20:45:17 -0400

    First test success on removeArray question

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+let array = [];
 
+const removeFromArray = function (array, poppedNum) {
+  let index = array.indexOf(poppedNum);
+  if (index >= 0) {
+    array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
